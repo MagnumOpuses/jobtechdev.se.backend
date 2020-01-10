c14757b9bd77958d06aa6616f58f2c38d5f5fec2
Mats Löfstrand <mats.lofstrand@arbetsformedlingen.se>
2020-01-10 11:13:44 +0100

    .

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var url = require('url');
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://jobtechdev.se");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
